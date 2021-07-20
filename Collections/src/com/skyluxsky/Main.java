bd0d1de2696ae6ec2b1643baf206ba95ca33e326
SkyluxSky <skyluxsky@gmail.com>
2021-07-20 19:00:12 -0400

    Comparable and Comparator for Sorting  (Remember -1,1,0 Assignments Help Sorting Algorithms)

--- Collections/src/com/skyluxsky/Main.java
+++ Collections/src/com/skyluxsky/Main.java
@@ -16,14 +16,51 @@ public class Main {
 
 
         Theatre theatre = new Theatre("Olympian",8,12);
-        theatre.getSeats();
 
-        //Brute Force Algorithm
-        if (theatre.reserveSeatBruteForce("G11")){
-            System.out.println("Please pay");
+        //Tests For Comparable and Comparator
+        if (theatre.reserveSeatBinaryTree("D12")){
+            System.out.println("Please pay for D12");
         } else {
             System.out.println("Sorry, seat is taken");
         }
+
+        if (theatre.reserveSeatBinaryTree("B13")){
+            System.out.println("Please pay for B13");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+
+        if (theatre.reserveSeatBinaryTree("D12")){
+            System.out.println("Please pay for D12");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+
+        if (theatre.reserveSeatBinaryTree("B13")){
+            System.out.println("Please pay for B13");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+
+        //Shows that Comparative method is working as intended, even with a reverse list
+        List<Theatre.Seat> reverseSeats = new ArrayList<>(theatre.getSeats());
+        Collections.reverse(reverseSeats);
+        printList(reverseSeats);
+
+        List<Theatre.Seat> priceSeats = new ArrayList<>(theatre.getSeats());
+        priceSeats.add(theatre.new Seat("B00",13.00));
+        priceSeats.add(theatre.new Seat("A00",13.00));
+        Collections.sort(priceSeats,theatre.PRICE_ORDER);
+        printList(priceSeats);
+
+        //theatre.getSeats();
+
+        //Brute Force Algorithm
+//        if (theatre.reserveSeatBruteForce("G11")){
+//            System.out.println("Please pay");
+//        } else {
+//            System.out.println("Sorry, seat is taken");
+//        }
 
         //Error Seat is Taken
 //        if (theatre.reserveSeatBruteForce("H11")){
@@ -33,71 +70,71 @@ public class Main {
 //        }
 
 
-        //Binary Tree Algorithm
-        if (theatre.reserveSeatBinaryTree("H12")){
-            System.out.println("Please pay");
-        } else {
-            System.out.println("Sorry, seat is taken");
-        }
-
-
-        //Java Binary Tree Algorithm
-        if (theatre.reserveSeatBinarySearchJava("E06")){
-            System.out.println("Please pay");
-        } else {
-            System.out.println("Sorry, seat is taken");
-        }
-
-/*******************************************************************************************/
-
+//        //Binary Tree Algorithm
+//        if (theatre.reserveSeatBinaryTree("H12")){
+//            System.out.println("Please pay");
+//        } else {
+//            System.out.println("Sorry, seat is taken");
+//        }
+//
+//
+//        //Java Binary Tree Algorithm
+//        if (theatre.reserveSeatBinarySearchJava("E06")){
+//            System.out.println("Please pay");
+//        } else {
+//            System.out.println("Sorry, seat is taken");
+//        }
+//
+///*******************************************************************************************/
+//
         //Copy Array List -Shallow Copy
-        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
-        printList(seatCopy);//Copy
-
-        //Copied arraylist still modifies when functions are called on itself
-        //Example of two references pointing to the same list/array
-        //List #1
-        seatCopy.get(1).reserve();
-        //List #2
-        if (theatre.reserveSeatBinarySearchJava("A02")){
-            System.out.println("Please Pay For A02");
-        } else {
-            System.out.println("Seat already reserved");
-        }
-
-
-        //Collection modification (Proves that both are separate lists)
-        //Collections.reverse(seatCopy); //reverses list
-        Collections.shuffle(seatCopy); //shuffles list (Pseudo Random)
-        System.out.println("Printing seatCopy");
-        printList(seatCopy);
-        System.out.println("Printing theatre.seats");
-        printList(theatre.seats);
+        //List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats);
+        //printList(seatCopy);//Copy
+//
+//        //Copied arraylist still modifies when functions are called on itself
+//        //Example of two references pointing to the same list/array
+////        //List #1
+//        seatCopy.get(1).reserve();
+//        //List #2
+//        if (theatre.reserveSeatBinaryTree("D12")){
+//            System.out.println("Please Pay For D12");
+//        } else {
+//            System.out.println("Seat already reserved");
+//        }
 
 
-        //Collections Min and Max Values
-        Theatre.Seat minSeat = Collections.min(seatCopy);
-        Theatre.Seat maxSeat = Collections.max(seatCopy);
-        System.out.println("Min seat number is " + minSeat.getSeatNumber());
-        System.out.println("Max seat number is " + maxSeat.getSeatNumber());
-
-        //Prints Sorted List
-        System.out.println();
-        sortList(seatCopy);
-        System.out.println("Printing sorted seatCopy");
-        printList(seatCopy);
-
-
-        //Collections Copy List
-        List<Theatre.Seat> newList = new ArrayList<>(theatre.seats.size());
-        Collections.copy(newList, theatre.seats);
+//        //Collection modification (Proves that both are separate lists)
+//        //Collections.reverse(seatCopy); //reverses list
+//        Collections.shuffle(seatCopy); //shuffles list (Pseudo Random)
+//        System.out.println("Printing seatCopy");
+//        printList(seatCopy);
+//        System.out.println("Printing theatre.seats");
+//        printList(theatre.seats);
+//
+//
+//        //Collections Min and Max Values
+//        Theatre.Seat minSeat = Collections.min(seatCopy);
+//        Theatre.Seat maxSeat = Collections.max(seatCopy);
+//        System.out.println("Min seat number is " + minSeat.getSeatNumber());
+//        System.out.println("Max seat number is " + maxSeat.getSeatNumber());
+//
+//        //Prints Sorted List
+//        System.out.println();
+//        sortList(seatCopy);
+//        System.out.println("Printing sorted seatCopy");
+//        printList(seatCopy);
+//
+//
+//        //Collections Copy List
+//        List<Theatre.Seat> newList = new ArrayList<>(theatre.seats.size());
+//        Collections.copy(newList, theatre.seats);
     }
 
 
     //Creates a new Array of Seat Objects (Uses Inner Class Notations)
     public static void printList(List<Theatre.Seat> list){
         for (Theatre.Seat seat : list){
-            System.out.println(" " + seat.getSeatNumber());
+            System.out.println(" " + seat.getSeatNumber() + " " + seat.getPrice());
         }
         System.out.println();
         System.out.println("============================================================");
@@ -106,13 +143,13 @@ public class Main {
 
     //Bubble Sort
     //List<? extends Theatre.Seat> makes our argument generic - method can sort any theatre that use compareTo method
-    public static void sortList(List<? extends Theatre.Seat> list){
-        for (int i = 0; i < list.size() - 1; i++){
-            for (int j = i + 1; j < list.size(); j++){
-                if (list.get(i).compareTo(list.get(j)) > 0){
-                    Collections.swap(list, i , j);
-                }
-            }
-        }
-    }
+//    public static void sortList(List<? extends Theatre.Seat> list){
+//        for (int i = 0; i < list.size() - 1; i++){
+//            for (int j = i + 1; j < list.size(); j++){
+//                if (list.get(i).compareTo(list.get(j)) > 0){
+//                    Collections.swap(list, i , j);
+//                }
+//            }
+//        }
+//    }
 }
--- Collections/src/com/skyluxsky/Theatre.java
+++ Collections/src/com/skyluxsky/Theatre.java
@@ -8,7 +8,25 @@ public class Theatre {
 
     //The collections framework contains many different data structures. Many of these are interchangeable
     //This includes ArrayLists, LinkedLists, HashSets, and LinkedHashSets.
-    public List<Seat> seats = new ArrayList<>();
+    private List<Seat> seats = new ArrayList<>();
+
+    //Comparator Example - Anonymous Inner Class
+    static final Comparator<Seat> PRICE_ORDER;//Semi Colon Expected
+
+    static {
+        PRICE_ORDER = new Comparator<Seat>() {
+            @Override
+            public int compare(Seat seat1, Seat seat2) {
+                if (seat1.getPrice() < seat2.getPrice()) {
+                    return -1;
+                } else if (seat1.getPrice() > seat2.getPrice()) {
+                    return 1;
+                } else {
+                    return 0;
+                }
+            }
+        };
+    }
 
     //Constructor
     //numRows = number of rows of seats
@@ -20,8 +38,21 @@ public class Theatre {
         //Goes through and allocates seats for that section
         for (char row = 'A'; row <= lastRow; row++){
             for (int seatNum = 1; seatNum <= seatsPerRow; seatNum++){
+
+                //====================================================
+                //Manage Prices:
+                double price = 12.00;
+
+                if ((row < 'D') && (seatNum >= 4 && seatNum <= 9)){
+                    price = 14.00; //raised price
+                } else if (row > 'F' || (seatNum < 4 || seatNum > 9)){
+                    price = 7.00;//discount
+                }
+                //====================================================
+
+
                 //Format provides format where 0 is always in front of the declared value
-                Seat seat = new Seat(row + String.format("%02d",seatNum));//EX A01, B01, C01, C02 etc... (As String)
+                Seat seat = new Seat(row + String.format("%02d",seatNum), price);//EX A01, B01, C01, C02 etc... (As String)
                 seats.add(seat);
             }
         }
@@ -59,7 +90,7 @@ public class Theatre {
 
     //Binary Search Algorithm (O log(n))
     public boolean reserveSeatBinaryTree(String seatNumber){
-        Seat requestedSeat = new Seat(seatNumber);//Gives us object for comparison purposes
+        Seat requestedSeat = new Seat(seatNumber, 0);//Gives us object for comparison purposes
         int foundSeat = Collections.binarySearch(seats, requestedSeat, null); //uses comparable interface to compare requestedseat to the seats.
 
         if (foundSeat >= 0){
@@ -73,7 +104,7 @@ public class Theatre {
 
     //Example of Binary Tree From Java's Library
     //More Modifiable than Code Above...
-    public boolean reserveSeatBinarySearchJava(String seatNumber){
+    private boolean reserveSeatBinarySearchJava(String seatNumber){
         int low = 0;
         int high = seats.size()-1; //Last Element - 1
 
@@ -99,21 +130,24 @@ public class Theatre {
  /*****************************************************************************************/
 
     //For Testing:
-    public void getSeats(){
-        for (Seat seat : seats){
-            System.out.println(seat.getSeatNumber());
-        }
+    public Collection<Seat> getSeats(){
+        return seats;
+//        for (Seat seat : seats){
+//            System.out.println(seat.getSeatNumber());
+//        }
     }
 
 
-    /**Inner class for Theatre Seats**/
+/*******************************************Inner class for Theatre Seats******************************/
     //We can improve performance by implementing a binary tree...
     public class Seat implements Comparable<Seat> {
         private final String seatNumber;
+        private double price;
         private boolean reserved = false;//Reserved True or False
 
-        public Seat(String seatNumber) {
+        public Seat(String seatNumber, double price) {
             this.seatNumber = seatNumber;
+            this.price = price;
         }
 
         //reserves a seat
@@ -143,6 +177,9 @@ public class Theatre {
             return seatNumber;
         }
 
+        public double getPrice() {
+            return price;
+        }
 
         //Sets up values for Binary Tree
         @Override
@@ -152,6 +189,4 @@ public class Theatre {
             return this.seatNumber.compareToIgnoreCase(seat.getSeatNumber());
         }
     }
-
-
 }
