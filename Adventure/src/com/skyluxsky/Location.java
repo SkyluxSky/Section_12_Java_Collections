2abeee2043ee39b83188f7055b7187156dbee7e2
SkyluxSky <skyluxsky@gmail.com>
2021-07-23 14:13:24 -0400

    Intro to Immutable Classes

--- Adventure/src/com/skyluxsky/Location.java
+++ Adventure/src/com/skyluxsky/Location.java
@@ -6,22 +6,25 @@ import java.util.Map;
 //Location in an adventure game
 public class Location {
     //Fields
+    //Private final - allows reader to understand that code should not be changed
     private final int locationID;
     private final String description;
     private final Map<String, Integer> exits;
 
     //Constructor
-    public Location(int locationID, String description) {
+    public Location(int locationID, String description, Map<String, Integer> exits) {
         this.locationID = locationID;
         this.description = description;
-        this.exits = new HashMap<String, Integer>();
+        this.exits = new HashMap<String, Integer>(exits); //Creates another new hashmap (based on hashmap passed from main method)
         this.exits.put("Q",0);//Adds location to the code that allows for user to Quit
     }
 
     //Methods
-    public void addExit(String direction, int location){
-        exits.put(direction,location); //Stores specified exit
-    }
+
+    //Displays Put method (Automatically configured in constructor)
+//    public void addExit(String direction, int location){
+//        exits.put(direction,location); //Stores specified exit
+//    }
 
     public int getLocationID() {
         return locationID;
@@ -31,6 +34,7 @@ public class Location {
         return description;
     }
 
+    //Creates duplicate of exits Map to ensure the safety of original exits Map in field.
     public Map<String, Integer> getExits() {
         return new HashMap<String, Integer>(exits); //A new map is created, but contains all exits from the original exits map
     }
--- Adventure/src/com/skyluxsky/Main.java
+++ Adventure/src/com/skyluxsky/Main.java
@@ -8,6 +8,10 @@ package com.skyluxsky;
 //
 // Single letter commands (N, W, S, E, Q) should still be available.
 
+/**Immutable Classes**/
+//Protect classes and programs from third parties.
+
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -19,38 +23,47 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         //Default Locations
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building"));
-        locations.put(2, new Location(2, "You are at the top of a hill"));
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
-        locations.put(4, new Location(4, "You are in a valley beside a stream"));
-        locations.put(5, new Location(5, "You are in the forest"));
+        //Each time constructor is called, it passes the list of exits to the constructor. (Reuses and updates tempExit Map)
+        Map<String, Integer> tempExit = new HashMap<String, Integer>();
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",tempExit));
 
+        //Temporary Map
+        tempExit = new HashMap<String, Integer>();
         //Add Exits - No Map Literals in Java
         //Room 1 - Road and Rooms it connects to
-        locations.get(1).addExit("W",2);
-        locations.get(1).addExit("E",3);
-        locations.get(1).addExit("S",4);
-        locations.get(1).addExit("N",5);
-        //locations.get(1).addExit("Q",0);
+        tempExit.put("W",2);
+        tempExit.put("E",3);
+        tempExit.put("S",4);
+        tempExit.put("N",5);
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building",tempExit));
+        //tempExit.put("Q",0);
 
         //Room 2 - Hill
-        locations.get(2).addExit("N",5);
-        //locations.get(2).addExit("Q",0);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("N",5);
+        locations.put(2, new Location(2, "You are at the top of a hill",tempExit));
+        //tempExit.put("Q",0);
 
         //Room 3 - Building
-        locations.get(3).addExit("W",1);
-        //locations.get(3).addExit("Q",0);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("W",1);
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring",tempExit));
+        //tempExit.put("Q",0);
 
         //Room 4 - Valley
-        locations.get(4).addExit("N",1);
-        locations.get(4).addExit("W",2);
-        //locations.get(4).addExit("Q",0);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("N",1);
+        tempExit.put("W",2);
+        locations.put(4, new Location(4, "You are in a valley beside a stream",tempExit));
+        //tempExit.put("Q",0);
 
         //Room 5 - Forest
-        locations.get(5).addExit("S",1);
-        locations.get(5).addExit("W",2);
-        //locations.get(5).addExit("Q",0);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("S",1);
+        tempExit.put("W",2);
+        locations.put(5, new Location(5, "You are in the forest",tempExit));
+
+        //tempExit.put("Q",0);
 
         Map<String, String> vocabulary = new HashMap<String, String>();
         vocabulary.put("QUIT","Q");
@@ -64,6 +77,8 @@ public class Main {
         while (true){
             //Quits game if location 0
             System.out.println(locations.get(loc).getDescription());
+            //Issue: Direction can still be removed, as there are direct references to original class.
+            //tempExit.remove("S");
             if (loc == 0){
                 break;
             }
@@ -117,10 +132,5 @@ public class Main {
             System.out.println(i);
         }
     }
-
-    public static void command(){
-
-    }
-
-
+    
 }
