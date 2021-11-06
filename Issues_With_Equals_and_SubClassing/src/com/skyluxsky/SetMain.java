eb20450486a99fd63b11dc6dbde6456b67aa8597
SkyluxSky <skyluxsky@gmail.com>
2021-11-05 21:50:08 -0400

    Completed Java Sets - Unions and Intersections

--- Issues_With_Equals_and_SubClassing/src/com/skyluxsky/SetMain.java
+++ Issues_With_Equals_and_SubClassing/src/com/skyluxsky/SetMain.java
@@ -47,9 +47,50 @@ public class SetMain {
         //comparing two sets of strings
         Set<String> nature = new HashSet<>();
         Set<String> divine = new HashSet<>();
+        //There is no way to create set literals in Java...
         String[] natureWords = {"all","nature","is","but","art","unknown","to","thee"};
         nature.addAll(Arrays.asList(natureWords));
+
         String[] divineWords = {"to","err","is","human","to","forgive","is","divine"};
         divine.addAll(Arrays.asList(divineWords));
+
+        System.out.println("nature - divine");
+        Set<String> diff1 = new HashSet<>(nature);
+        diff1.removeAll(divine);
+        printSet(diff1);
+
+        System.out.println("divine - nature");
+        Set<String> diff2 = new HashSet<>(nature);
+        printSet(diff2);
+
+        Set<String> unionTest = new HashSet<>(nature);
+        unionTest.addAll(divine);
+        Set<String> intersectionTest = new HashSet<>(nature);
+        intersectionTest.retainAll(divine);
+
+        System.out.println("Symmetric Difference");
+        unionTest.removeAll(intersectionTest);
+        printSet(unionTest);
+
+        if (nature.containsAll(divine)){
+            System.out.println("divine is a subset of nature.");
+        }
+
+        if (nature.containsAll(intersectionTest)){
+            System.out.println("Intersection is a subset of nature.");
+        }
+
+        if(divine.containsAll(intersectionTest)){
+            System.out.println("Intersection is a subset of divine.");
+        }
+
+    }
+
+    private static void printSet(Set<String> set){
+        System.out.println('\t');
+        for (String s : set){
+            System.out.println(s + " ");
+        }
+        System.out.println();
     }
 }
