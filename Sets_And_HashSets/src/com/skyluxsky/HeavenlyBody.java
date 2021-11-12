accab13782ba0ebfe2d2e88cdbb2f8b6d035c674
SkyluxSky <skyluxsky@gmail.com>
2021-11-11 20:07:06 -0500

    Set challenge completed... Inner class Key Created and HeavenlyBody now Abstract and immutable

--- Sets_And_HashSets/src/com/skyluxsky/HeavenlyBody.java
+++ Sets_And_HashSets/src/com/skyluxsky/HeavenlyBody.java
@@ -4,10 +4,11 @@ import java.util.HashSet;
 import java.util.Set;
 
 public abstract class HeavenlyBody {
-    private final String name;
+    private final Key key;
+    //private final String name;
     private final double orbitalPeriod;
     private final Set<HeavenlyBody> satellites;
-    private final BodyTypes bodyType; // The type of celestial body the object is
+    //private final BodyTypes bodyType; // The type of celestial body the object is
 
     //Define an enum - allows for a list of constants to be defined.
     //Enums allows for checking for errors at compile time!!!
@@ -21,27 +22,32 @@ public abstract class HeavenlyBody {
     }
 
     public HeavenlyBody(String name, double orbitalPeriod, BodyTypes bodyType) {
-        this.name = name;
+        this.key = new Key(name, bodyType);
+        //this.name = name;
         this.orbitalPeriod = orbitalPeriod;
         //initialize Hashset Class
         this.satellites = new HashSet<>();
-        this.bodyType = bodyType; //Sets bodytype as value from enumerated list of constants(Only accepts values in enumerated list).
+        //this.bodyType = bodyType; //Sets bodytype as value from enumerated list of constants(Only accepts values in enumerated list).
     }
 
     //Methods
     //Getter for name
-    public String getName() {
-        return name;
-    }
+//    public String getName() {
+//        return name;
+//    }
 
     //Getter for orbital period
     public double getOrbitalPeriod() {
         return orbitalPeriod;
     }
 
-    public BodyTypes getBodyType() {
-        return bodyType;
+    public Key getKey() {
+        return key;
     }
+
+    //    public BodyTypes getBodyType() {
+//        return bodyType;
+//    }
 
     public boolean addSatellite(HeavenlyBody moon){
             return this.satellites.add(moon);
@@ -73,9 +79,7 @@ public abstract class HeavenlyBody {
         //
         if (obj instanceof HeavenlyBody){
             HeavenlyBody theObject = (HeavenlyBody) obj;
-            if (this.name.equals(theObject.getName())){
-                return this.bodyType == theObject.getBodyType();
-            }
+            return this.key.equals(theObject.getKey());
         }
 
         //Casts the name to a heavenly body datatype.
@@ -91,13 +95,62 @@ public abstract class HeavenlyBody {
         //System.out.println("Hashcode Called.");
 
         //Each enumerated list Item has their own hashcode...
-        return this.name.hashCode() + 57 + this.bodyType.hashCode(); //Guarantees a non zero number is generated.
+        //return this.name.hashCode() + 57 + this.bodyType.hashCode(); //Guarantees a non zero number is generated.
 
+        //Returns hashcode from Key class
+        return this.key.hashCode();
+    }
+
+    //Allows for us to search hashmap with key
+    public static Key makeKey(String name, BodyTypes bodyType){
+        return new Key(name, bodyType);
     }
 
     //Override toString - Java will call a toString to call filed items as Strings.
     @Override
     public String toString() {
-        return this.name + ": " + this.bodyType + ", " + this.orbitalPeriod;
+        return this.key.name + ": " + this.key.bodyType + ", " + this.orbitalPeriod;
+    }
+
+
+    //Addresses issue with having bodytype and name paired.
+    public static final class Key{
+        private String name;
+        private BodyTypes bodyType;
+
+        private Key(String name, BodyTypes bodyType){
+            this.name = name;
+            this.bodyType = bodyType;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public BodyTypes getBodyType() {
+            return bodyType;
+        }
+
+        @Override
+        public int hashCode() {
+            //Creates unique hashcode
+            return this.name.hashCode() + 57 + this.bodyType.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Key key = (Key) obj;
+
+            //Double checks to make sure passed object is equal.
+            if(this.name.equals(key.getName())){
+                return (this.bodyType == key.getBodyType());
+            }
+            return false;
+        }
+
+        @Override
+        public String toString() {
+            return this.name + ": " + this.bodyType;
+        }
     }
 }
--- Sets_And_HashSets/src/com/skyluxsky/Main.java
+++ Sets_And_HashSets/src/com/skyluxsky/Main.java
@@ -8,7 +8,7 @@ import java.util.Set;
 public class Main {
 
     //Creates new HashMap and initialize it
-    private static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+    private static Map<HeavenlyBody.Key, HeavenlyBody> solarSystem = new HashMap<>();
     //Creates new HashSet and initialize it
     private static Set<HeavenlyBody> planets = new HashSet<>();
 
@@ -17,69 +17,69 @@ public class Main {
         //Sets do not contain duplicate values.
         //You cannot retrieve a specific element of a set
         HeavenlyBody temp = new Planet("Mercury", 88);
-        solarSystem.put(temp.getName(),temp);
+        solarSystem.put(temp.getKey(),temp);
         planets.add(temp);
 
         temp = new Planet("Venus", 88);
-        solarSystem.put(temp.getName(),temp);
+        solarSystem.put(temp.getKey(),temp);
         planets.add(temp);
 
         temp = new Planet("Earth", 88);
-        solarSystem.put(temp.getName(),temp);
+        solarSystem.put(temp.getKey(),temp);
         planets.add(temp);
 
         HeavenlyBody tempMoon = new Moon("Moon", 27);
-        solarSystem.put(tempMoon.getName(),tempMoon);
+        solarSystem.put(tempMoon.getKey(),tempMoon);
         //Adds moon to the last planet (Which is Earth).
         temp.addSatellite(tempMoon);
 
         temp = new Planet("Mars", 687);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         tempMoon = new Moon("Deimos", 1.3);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellite(tempMoon); // temp is still Mars
 
         tempMoon = new Moon("Phobos", 0.3);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellite(tempMoon); // temp is still Mars
 
         temp = new Planet("Jupiter", 4332);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         //Temp is Jupiter (so all moons of Jupiter are added to temp)
         tempMoon = new Moon("Io", 1.8);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellite(tempMoon); // temp is still Jupiter
 
         tempMoon = new Moon("Europa", 3.5);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellite(tempMoon); // temp is still Jupiter
 
         tempMoon = new Moon("Ganymede", 7.1);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellite(tempMoon); // temp is still Jupiter
 
         tempMoon = new Moon("Callisto", 16.7);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellite(tempMoon); // temp is still Jupiter
 
         temp = new Planet("Saturn", 10759);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         temp = new Planet("Uranus", 30660);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         temp = new Planet("Neptune", 165);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         temp = new Planet("Pluto", 248);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
 
@@ -88,15 +88,15 @@ public class Main {
         System.out.println("Planets");
         //For each planet in planets Set
         for(HeavenlyBody planet : planets) {
-            System.out.println("\t" + planet.getName());
+            System.out.println("\t" + planet.getKey());
         }
 
         //Prints out moons of planets
-        HeavenlyBody body = solarSystem.get("Jupiter");
-        System.out.println("Moons of " + body.getName());
+        HeavenlyBody body = solarSystem.get(HeavenlyBody.makeKey("Mars", HeavenlyBody.BodyTypes.PLANET));
+        System.out.println("Moons of " + body.getKey());
         //For each moon in that satellites set.
         for(HeavenlyBody jupiterMoon: body.getSatellites()) {
-            System.out.println("\t" + jupiterMoon.getName());
+            System.out.println("\t" + jupiterMoon.getKey());
         }
 
 
@@ -114,7 +114,7 @@ public class Main {
         //Print out moons with for each iterator
         System.out.println("All Moons");
         for(HeavenlyBody moon : moons) {
-            System.out.println("\t" + moon.getName());
+            System.out.println("\t" + moon.getKey());
         }
 
         //Add additional pluto (A set cannot contain two of the same object)
@@ -126,7 +126,7 @@ public class Main {
 
         System.out.println("To String Function: Converts all passed field values to strings");
         for (HeavenlyBody planet : planets){
-            //System.out.println(planet.getName() + ": " + planet.getOrbitalPeriod());
+            //System.out.println(planet.getKey() + ": " + planet.getOrbitalPeriod());
             System.out.println(planet);
         }
 
@@ -143,5 +143,16 @@ public class Main {
         System.out.println(earth1.equals((pluto)));
         System.out.println(pluto.equals(earth1));
 
+        //Test - Confirms that two different objects of the same name are in a Set
+        solarSystem.put(pluto.getKey(), pluto);
+        System.out.println(solarSystem.get(HeavenlyBody.makeKey("Pluto", HeavenlyBody.BodyTypes.PLANET)));
+        System.out.println(solarSystem.get(HeavenlyBody.makeKey("Pluto", HeavenlyBody.BodyTypes.DWARF_PLANET)));
+
+
+        System.out.println();
+        System.out.println("The solar system contains");
+        for (HeavenlyBody heavenlyBody : solarSystem.values()){
+            System.out.println(heavenlyBody);
+        }
     }
 }
